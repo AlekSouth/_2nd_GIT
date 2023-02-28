139f29c5515f1be1e7e975e3b89022aba2b81863
Alek <themain89@mail.ru>
2023-02-28 23:50:26 +0300

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       SalesManager salesManager = new SalesManager(new int[] {15, 100, 500, 150});
+       int maxSale = salesManager.max();
+        System.out.println(maxSale);
+
+
+
+
+        // System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+    }
+}
